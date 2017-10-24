80191e69230835b452d21a2668868b8ac98fd102
ali.azaz <openmind_ali@hotmail.com>
2017-10-24 14:00:06 +0500

    COMMIT

--- app/src/main/java/edu/aku/hassannaqvi/wfpstuntingpishin/activities/SectionKBIYCFActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/wfpstuntingpishin/activities/SectionKBIYCFActivity.java
@@ -11,6 +11,6 @@ public class SectionKBIYCFActivity extends Activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_section_kbiycf);
+        setContentView(R.layout.activity_section_kbIYCF);
     }
 }
